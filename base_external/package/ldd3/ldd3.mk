541c2cd7e88288f76a0665fcd61839f534e43fc2
Marko Hammersen <marko.hammersen@gmail.com>
2025-11-26 17:30:45 +0100

    assignment7-buildroot-complete

--- base_external/package/ldd3/ldd3.mk
+++ base_external/package/ldd3/ldd3.mk
@@ -7,7 +7,7 @@
 
 LDD3_VERSION = 8cfea8e94f45e494907c31ab9e3f597fad7c229e
 LDD3_SITE = git@github.com:cu-ecen-aeld/assignment-7-MarkoHammersen.git
-LDD3_OVERRIDE_SRCDIR = /home/marko/Documents/Linux_UCB_1/assignment-7-MarkoHammersen
+#LDD3_OVERRIDE_SRCDIR = /home/marko/Documents/Linux_UCB_1/assignment-7-MarkoHammersen
 LDD3_SITE_METHOD = git
 LDD3_GIT_SUBMODULES = YES
 LDD3_MODULE_SUBDIRS= misc-modules scull
